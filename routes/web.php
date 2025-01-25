d72b8c0b69a1865d2215e9a9b29a9100dfedce20
unknown <wildannoovnooob@gmail.com>
2025-01-25 23:40:28 +0700

    web rout

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Nilai;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\GuruController;
+use App\Http\Controllers\Admin\NilaiController;
+use App\Http\Controllers\Admin\SiswaController;
+use App\Http\Controllers\Admin\TugasController;
+use App\Http\Controllers\Admin\MateriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('landing.index');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -19,7 +27,12 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->group(
     function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::resource('guru', GuruController::class)->middleware(Admin::class);
+        Route::resource('kelas', KelasController::class)->middleware(Admin::class);
+        Route::resource('materi', MateriController::class)->middleware(Admin::class);
+        Route::resource('nilai', NilaiController::class)->middleware(Admin::class);
+        Route::resource('siswa', SiswaController::class)->middleware(Admin::class);
+        Route::resource('Tugas', TugasController::class)->middleware(Admin::class);
     });
 
 require __DIR__.'/auth.php';
