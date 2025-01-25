3a628a1db8e3076351559872abe8ed83c89816c7
Jujunzy <arjunaprasetya63@gmail.com>
2025-01-25 23:54:46 +0700

    hali

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('landing.index');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
