2e6a1917e6334fd3954dc2c93c3802cabaf2562d
argafirman <argafirmanmaulana17@gmail.com>
2025-01-27 22:40:22 +0700

    m

--- database/migrations/2025_01_25_155857_create_nilais_table.php
+++ database/migrations/2025_01_25_155857_create_nilais_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,10 +12,10 @@ return new class extends Migration
     {
         Schema::create('nilai', function (Blueprint $table) {
             $table->id();
-            $table->integer('nilai');    
-            $table->foreignId('guru_id')->constrained('guru')->onDelete('cascade'); 
+            $table->integer('nilai');
+            $table->foreignId('guru_id')->constrained('guru')->onDelete('cascade');
             $table->foreignId('siswa_id')->constrained('siswa')->onDelete('cascade');
-            $table->foreignId('tugas_id')->constrained('tugas')->onDelete('cascade'); 
+            $table->foreignId('tugas_id')->constrained('tugas')->onDelete('cascade');
             $table->timestamps();
         });
 
