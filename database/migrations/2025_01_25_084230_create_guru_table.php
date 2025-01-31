29d51cb0d9d10cda828b2772efa9bcc780e63ad1
argafirman <argafirmanmaulana17@gmail.com>
2025-01-31 07:32:30 +0700

    error guru

--- database/migrations/2025_01_25_084230_create_guru_table.php
+++ database/migrations/2025_01_25_084230_create_guru_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('guru', function (Blueprint $table) {
             $table->id();
             $table->string('nama_guru')->unique();
-            $table->enum('status',['aktif','nonaktif']);
+            $table->enum('status', ['aktif', 'nonaktif']);
             $table->integer('umur');
             $table->string('alamat');
             $table->timestamps();
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('gurus');
+        Schema::dropIfExists('guru');
     }
 };
